1c5955110164e61018aaaeeb9411327df0f0139f
MargaritaBurova <margarita.burova210808@gmail.com>
2025-05-07 08:56:20 +0300

    ping

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+hosts= ["8.8.8.8","1.1.1.1", "192.168.1.1" ]
+while True:
+    print("kättesadavuse kontroll")
+    for elem in hosts:
+        response = os.system(f"ping -n 1 {elem} > null")
+        
+        if response == 0:
+            print(elem, "kätesadavalt")
+        else:
+            print(elem, "ei ole kättesadavalt")
+        print("-"*30)
+        time.sleep(5)
